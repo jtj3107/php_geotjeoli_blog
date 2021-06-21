24deb9fb02a3c9c84de4e3c535cb707ebafdae56
jtj3107 <jtj3926@gmail.com>
2021-06-21 16:09:47 +0900

    세팅3

--- public/app/service/HomeService.php
+++
@@ -1,9 +0,0 @@
-<?php
-class APP__HomeService {
-  private APP__HomeRepository $homeRepository;
-
-  public function __construct() {
-    global $App__homeRepository;
-    $this->homeRepository = $App__homeRepository;
-  }
-}
